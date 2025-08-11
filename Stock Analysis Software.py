a955d41e3b578b73d7613890c040caa043193346
femiajayi0707 <femiajayi0707@gmail.com>
2025-08-11 14:48:35 +0100

    Add stock analysis code

---
+++ Stock Analysis Software.py
@@ -0,0 +1,18 @@
+#Stock Analysis Software
+import requests
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from IPython.display import display
+import sklearn.linear_model as lm
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+
+def getMonthlyData ():
+    stockName = input("What is the name of the companies stock")
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=' + stockName + '&apikey=OYKT17X1F6JCW060'
+    r = requests.get(url)
+    data = r.json()
+
