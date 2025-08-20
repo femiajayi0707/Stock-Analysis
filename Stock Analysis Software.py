27cbd78264f8c389c16a6ea89d4ca5b0a31d9325
femiajayi0707 <femiajayi0707@gmail.com>
2025-08-20 15:26:08 +0100

    Update Stock Analysis Software.py

--- Stock Analysis Software.py
+++ Stock Analysis Software.py
@@ -10,22 +10,23 @@ import sklearn.linear_model as lm
 from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
-data = []
-
 def getMonthlyData ():
-    stockName = input("What is the name of the companies stock")
+    stockName = input("What is the name of the companies stock ")
     url = 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=' + stockName + '&apikey=OYKT17X1F6JCW060'
     r = requests.get(url)
     data = r.json()
+    return data
 
 def getWeeklyData ():
-    stockName = input("What is the name of the companies stock")
+    stockName = input("What is the name of the companies stock ")
     url = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol='  + stockName + '&apikey=OYKT17X1F6JCW060'
     r = requests.get(url)
     data = r.json()
+    return data
 
 def getDailyData ():
-    stockName = input("What is the name of the companies stock")
+    stockName = input("What is the name of the companies stock ")
     url = url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol='  + stockName + '&apikey=OYKT17X1F6JCW060'
     r = requests.get(url)
     data = r.json()
+    return data
